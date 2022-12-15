5b67aeab2742928600571f11dd6eb6ef4ba237ff
Donnie Plumly <donnie.plumly@gmail.com>
2022-12-15 11:37:25 -0800

    Timer function
    added font, added real timeout after 60 secs. Modal fade

--- js/timeout.js
+++ js/timeout.js
@@ -1,11 +1,8 @@
 let timeoutID;
 let clock = document.getElementById("timerWrapper");
+let modalBackground = document.getElementById("modal-background");
 let timeLeft = 15;
 let intervalId = null
-
-
-
-
 
 function setup() {
     // this.addEventListener("mousemove", resetTimer, false);
@@ -24,11 +21,18 @@ setup();
 
 function startTimer() {
     console.log('3');
-    timeoutID = setTimeout(goInactive, 2000);
+    timeoutID = setTimeout(goInactive, 60000);
 }
 
 function resetTimer(e) {
     clock.style.display = 'none';
+    modalBackground.style.display = 'none';
+
+    if (clock.style.display == 'none') {
+        clock.classList.remove('fade-in-modal')
+        clock.classList.add('fade-out-modal')
+    }
+
     clearInterval(intervalId);
     clearTimeout(timeoutID);
     console.log('4');
@@ -37,8 +41,16 @@ function resetTimer(e) {
 
 function goInactive() {
     intervalId = setInterval(() => {
-        document.getElementById("timer").innerHTML = String(timeLeft);
+        document.getElementById('timer').innerHTML = String(timeLeft);
+
         clock.style.display = 'block';
+        modalBackground.style.display = 'block';
+
+        if (clock.style.display == 'block') {
+            clock.classList.remove('fade-out-modal')
+            clock.classList.add('fade-in-modal')
+            modalBackground.classList.add('modal-bg-fade-in')
+        }
 
         if (timeLeft > 0) {
             console.log('5');
@@ -47,8 +59,10 @@ function goInactive() {
 
         if (timeLeft === 0) {
             console.log('reload');
+            window.location.reload(true);
         }
     }, 1000);
+
     timeLeft = 15;
     console.log('6');
 }
